c0382254d162e74c4a102f18ace58e88aa35d441
Sanca050601 <90603494+Sanca050601@users.noreply.github.com>
2021-10-04 15:50:02 +0200

    Add files via upload

---
+++ magassag.py
@@ -0,0 +1,28 @@
+print('Szia ' + input('Hogy hívnak? ') + '!')
+
+x = int(input ('Magasságod: '))
+
+y = int(180)
+
+z = int(170)
+
+a = int(171)
+b = int(172)
+c = int(173)
+d = int(174)
+e = int(174)
+f = int(175)
+g = int(176)
+h = int(177)
+j = int(178)
+k = int(179)
+
+print(x)
+
+
+if(x>y):
+    print('TE ZSIRÁF GECI')
+elif(x==y, z, a, b, c, d, e, f, g, h, j, k):
+    print('ÁTLAGOS')
+if(x<z):
+    print('TE TÖRPE GECI')
