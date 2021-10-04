dbdf4899ac1e23a90c3430bfe72c7a2e23f34ee9
Sanca050601 <90603494+Sanca050601@users.noreply.github.com>
2021-10-04 10:34:44 +0200

    Add files via upload

---
+++ adsfno.py
@@ -0,0 +1,22 @@
+print('1. feladat')
+
+print('Szia ' + input('Neved? ') + '!')
+
+print('**********')
+print('oooooooooo')
+
+szam_1 = 5
+szam_2 = 77
+
+print(szam_1 + szam_2)
+
+x = int(input('Szám1: '))
+y = int(input('Szám2: '))
+print(x+y)
+
+if(x>y):
+    print('Az első szám nagyobb!')
+elif(x==y):
+    print('A két szám egyenlő!')
+else:
+    print('A második szám a nagyobb!')
