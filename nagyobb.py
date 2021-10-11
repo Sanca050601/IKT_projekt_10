94035fb2ec675ec2a802237726097562a5f3bd1d
Sanca050601 <90603494+Sanca050601@users.noreply.github.com>
2021-10-11 09:35:29 +0200

    Add files via upload

---
+++ nagyobb.py
@@ -0,0 +1,11 @@
+x = int(input('Szám_1: '))
+y = int(input('Szám_2: '))
+
+if(x>y):
+    print('A nagyobb érték:', x)
+
+elif(x==y):
+    print('A két szám egyenlő')
+
+else:
+    print('A nagyobb érték:', y)
