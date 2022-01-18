d25ce64985fc8fa0094af6138ad2635b2941f2d9
Sanca050601 <90603494+Sanca050601@users.noreply.github.com>
2022-01-18 14:57:40 +0100

    Add files via upload

---
+++ nev.py
@@ -0,0 +1,39 @@
+while True:
+
+    nev = input('Add meg az egyik nevet: ')
+    
+    if(nev=='Alex'):
+        break
+    if(nev=='Sanyi'):
+        break
+    if(nev=='Martin'):
+        break
+    else:
+        print('HIBA')
+    
+while True:
+    
+    nev1 = input('Add meg a másik nevet: ')
+
+    if(nev1=='Martin'):
+        break
+    if(nev1=='Sanyi'):
+        break
+    if(nev1=='Alex'):
+        break
+    else:
+        print('HIBA')
+    
+while True:
+
+    nev2 = input('Add meg a harmadik nevet: ')
+    
+    if(nev2=='Sanyi'):
+        break
+    if(nev2=='Martin'):
+        break
+    if(nev2=='Alex'):
+        print('Sziasztok!')
+        break
+    else:
+        print('HIBA')
